925ad876db60a8e1204ba5f54264919c7fdda3e8
NumanArshad <afshanarsha2783@gmail.com>
2019-07-06 11:34:07 +0500

    Facebook and Google Integration

--- module03/FacebookIntergration/EmployeeManagementSystem/Models/LoginViewModel.cs
+++ module03/FacebookIntergration/EmployeeManagementSystem/Models/LoginViewModel.cs
@@ -7,7 +7,8 @@ namespace EmployeeManagementSystem.Models
 {
     public class LoginViewModel
     {
-        public string username { get; set; }
-        public string password { get; set; }
+     
+ public string username { get; set; }
+        public string password { get; set; }  
     }
 }
